010645862a94e1d71b7765458ac4c2fa65a48268
Wendy Robins <wendy@therobins.net>
2022-02-02 14:58:18 -0500

    updated guest list application

--- src/com/wendeveloper/guestlist/Main.java
+++ src/com/wendeveloper/guestlist/Main.java
@@ -3,35 +3,24 @@ package com.wendeveloper.guestlist;
 import java.util.Scanner;
 
 public class Main {
+    static String[] guests = new String[10];
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        String[] guests = new String[10];
-        Scanner scanner = new Scanner(System.in);
-
-        guests[0] = "Jacob";
-        guests[1] = "Edward";
-        guests[2] = "Rose";
-        guests[3] = "Molly";
-        guests[4] = "Christopher";
+//        guests[0] = "Jacob";
+//        guests[1] = "Edward";
+//        guests[2] = "Rose";
+//        guests[3] = "Molly";
+//        guests[4] = "Christopher";
 
         //will perform the task and then show the menu again so the user can continue interact with program
         do {
-            System.out.println();
-            System.out.println("1 - Display All Guests");
-            System.out.println("2 - Add Guest");
-            System.out.println("3 - Remove Guest");
-            System.out.println("4 - Exit the program");
-            System.out.println("Option: ");
-            int option = scanner.nextInt();
-            System.out.println();
+            displayGuests();
+            displayMenu();
+            int option = getOption();
 
-            if (option == 1) {
-                //display all guests
-                for (int i = 0; i < guests.length; i++) {
-                    System.out.println("guest: " + guests[i]);
-                }
-            }
-            else if(option == 2){
+            if(option == 1){
                 for (int i = 0; i < guests.length; i++) {
                     if (guests[i] == null) {
                         System.out.println("Name: ");
@@ -40,9 +29,8 @@ public class Main {
                         break;
                     }
                 }
-
             }
-            else if(option == 3){
+            else if(option == 2){
                 System.out.println("Name: ");
                 String name = scanner.next();
                 //find name in guests and delete it
@@ -55,11 +43,51 @@ public class Main {
                         break;
                     }
                 }
+
+                //temp array to reposition elements in array
+                String[] tempArray = new String[guests.length];
+                int tempIndex = 0;
+                for (int i = 0; i < guests.length; i++) {
+                    if (guests[i] != null){
+                        tempArray[tempIndex] = guests[i];
+                        tempIndex++;
+                    }
+                }
+                guests = tempArray;
             }
-            else if(option == 4){
+            else if(option == 3){
                 break;
             }
         } while(true);
         System.out.println("Exiting...");
+    }
+
+    public static void displayGuests(){
+        System.out.println("______________________________\n- Guests -\n");  // \n <--new line
+        boolean isEmpty = true;
+        for (int i = 0; i < guests.length; i++) {
+            if (guests[i] != null){
+                System.out.println(i+1 + ". " + (guests[i]));
+                isEmpty = false;
+            }
+        }
+        if (isEmpty){
+            System.out.println("Guest list is empty");
+        }
+    }
+
+    public static void displayMenu(){
+        System.out.println("______________________________\n- Menu -\n");  // \n <--new line
+        System.out.println("1 - Add Guest");
+        System.out.println("2 - Remove Guest");
+        System.out.println("3 - Exit the program");
+
+    }
+
+    public static int getOption(){
+        System.out.println("Option: ");
+        int option = scanner.nextInt();
+        System.out.println();
+        return option;
     }
 }
