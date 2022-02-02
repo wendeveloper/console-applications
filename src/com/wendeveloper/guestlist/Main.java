0b1c5cada9290eefd0219f938c901302dda1360c
Wendy Robins <wendy@therobins.net>
2022-02-02 15:06:28 -0500

    added an addGuest and removeGuest methods

--- src/com/wendeveloper/guestlist/Main.java
+++ src/com/wendeveloper/guestlist/Main.java
@@ -8,11 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        guests[0] = "Jacob";
-//        guests[1] = "Edward";
-//        guests[2] = "Rose";
-//        guests[3] = "Molly";
-//        guests[4] = "Christopher";
+        insertTestNames();
 
         //will perform the task and then show the menu again so the user can continue interact with program
         do {
@@ -21,39 +17,10 @@ public class Main {
             int option = getOption();
 
             if(option == 1){
-                for (int i = 0; i < guests.length; i++) {
-                    if (guests[i] == null) {
-                        System.out.println("Name: ");
-                        guests[i] = scanner.next();
-                        //once name has been added, exit back to main
-                        break;
-                    }
-                }
+                addGuest();
             }
             else if(option == 2){
-                System.out.println("Name: ");
-                String name = scanner.next();
-                //find name in guests and delete it
-                for (int i = 0; i < guests.length; i++) {
-
-                    // if the value at particular index is not null &&
-                    // if the value at particular index equals the name entered
-                    if (guests[i] != null && guests[i].equals(name) ){
-                        guests[i] = null;
-                        break;
-                    }
-                }
-
-                //temp array to reposition elements in array
-                String[] tempArray = new String[guests.length];
-                int tempIndex = 0;
-                for (int i = 0; i < guests.length; i++) {
-                    if (guests[i] != null){
-                        tempArray[tempIndex] = guests[i];
-                        tempIndex++;
-                    }
-                }
-                guests = tempArray;
+                removeGuest();
             }
             else if(option == 3){
                 break;
@@ -81,7 +48,6 @@ public class Main {
         System.out.println("1 - Add Guest");
         System.out.println("2 - Remove Guest");
         System.out.println("3 - Exit the program");
-
     }
 
     public static int getOption(){
@@ -89,5 +55,50 @@ public class Main {
         int option = scanner.nextInt();
         System.out.println();
         return option;
+    }
+
+    public static void addGuest() {
+        for (int i = 0; i < guests.length; i++) {
+            if (guests[i] == null) {
+                System.out.println("Name: ");
+                guests[i] = scanner.next();
+                //once name has been added, exit back to main
+                break;
+            }
+        }
+    }
+
+    public static void removeGuest(){
+        System.out.println("Name: ");
+        String name = scanner.next();
+        //find name in guests and delete it
+        for (int i = 0; i < guests.length; i++) {
+
+            // if the value at particular index is not null &&
+            // if the value at particular index equals the name entered
+            if (guests[i] != null && guests[i].equals(name) ){
+                guests[i] = null;
+                break;
+            }
+        }
+
+        //temp array to reposition elements in array
+        String[] tempArray = new String[guests.length];
+        int tempIndex = 0;
+        for (int i = 0; i < guests.length; i++) {
+            if (guests[i] != null){
+                tempArray[tempIndex] = guests[i];
+                tempIndex++;
+            }
+        }
+        guests = tempArray;
+    }
+
+    public static void insertTestNames(){
+        guests[0] = "Jacob";
+        guests[1] = "Edward";
+        guests[2] = "Rose";
+        guests[3] = "Molly";
+        guests[4] = "Christopher";
     }
 }
