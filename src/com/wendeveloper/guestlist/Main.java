1faaa8c44559a0b010de9f28d457fd5c00bd2540
Wendy Robins <wendy@therobins.net>
2022-02-02 19:32:17 -0500

    insertGuest method

--- src/com/wendeveloper/guestlist/Main.java
+++ src/com/wendeveloper/guestlist/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
         insertTestNames();
 
-        //will perform the task and then show the menu again so the user can continue interact with program
+        //perform task and then show menu again so the user can continue interacting with program
         do {
             displayGuests();
             displayMenu();
@@ -19,17 +19,23 @@ public class Main {
             if(option == 1){
                 addGuest();
             }
-            else if(option == 2){
-                removeGuest();
+            else if(option  == 2){
+                insertGuest();
             }
             else if(option == 3){
+                renameGuest();
+            }
+            else if(option == 4){
+                removeGuest();
+            }
+            else if(option == 5){
                 break;
             }
         } while(true);
         System.out.println("Exiting...");
     }
 
-    public static void displayGuests(){
+    static void displayGuests(){
         System.out.println("______________________________\n- Guests -\n");  // \n <--new line
         boolean isEmpty = true;
         for (int i = 0; i < guests.length; i++) {
@@ -43,62 +49,99 @@ public class Main {
         }
     }
 
-    public static void displayMenu(){
+    static void displayMenu(){
         System.out.println("______________________________\n- Menu -\n");  // \n <--new line
         System.out.println("1 - Add Guest");
-        System.out.println("2 - Remove Guest");
-        System.out.println("3 - Exit the program");
+        System.out.println("2 - Insert Guest");
+        System.out.println("3 - Rename Guest");
+        System.out.println("4 - Remove Guest");
+        System.out.println("5 - Exit the program");
     }
 
-    public static int getOption(){
-        System.out.println("Option: ");
+    static int getOption(){
+        System.out.print("Option: ");
         int option = scanner.nextInt();
+        scanner.nextLine();
         System.out.println();
         return option;
     }
 
-    public static void addGuest() {
+    static void addGuest() {
         for (int i = 0; i < guests.length; i++) {
             if (guests[i] == null) {
-                System.out.println("Name: ");
-                guests[i] = scanner.next();
-                //once name has been added, exit back to main
+                System.out.print("Name: ");
+                guests[i] = scanner.nextLine();  //nextLine reads entire line, not just one word.
                 break;
             }
         }
     }
 
-    public static void removeGuest(){
-        System.out.println("Name: ");
-        String name = scanner.next();
-        //find name in guests and delete it
-        for (int i = 0; i < guests.length; i++) {
+    static void insertGuest(){
+        System.out.print("Number: ");
+        int num = scanner.nextInt();
+        scanner.nextLine();  // to consume a line
 
-            // if the value at particular index is not null &&
-            // if the value at particular index equals the name entered
-            if (guests[i] != null && guests[i].equals(name) ){
-                guests[i] = null;
-                break;
+        if (num >= 1 && num <= 10 && guests[num - 1] != null ) {  //if in range and is valid guest num
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+
+            for (int i = guests.length - 1; i > num - 1; i--) {  //take element before it and move it index currently on
+                //set index to 1 index less
+                guests[i] = guests[i - 1];
             }
+            guests[num - 1] = name;  //nextLine reads entire line, not just one word.
+        }
+        else {
+            System.out.println("\nError:  There is no guest with that number.");
         }
 
-        //temp array to reposition elements in array
-        String[] tempArray = new String[guests.length];
-        int tempIndex = 0;
-        for (int i = 0; i < guests.length; i++) {
-            if (guests[i] != null){
-                tempArray[tempIndex] = guests[i];
-                tempIndex++;
-            }
-        }
-        guests = tempArray;
+
+
     }
 
-    public static void insertTestNames(){
-        guests[0] = "Jacob";
-        guests[1] = "Edward";
-        guests[2] = "Rose";
-        guests[3] = "Molly";
-        guests[4] = "Christopher";
+    static void renameGuest(){
+        System.out.print("Number: ");
+        int num = scanner.nextInt();
+        scanner.nextLine();  // to consume a line
+
+        if (num >= 1 && num <= 10 && guests[num - 1] != null ) {  //if in range and is valid guest num
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+
+            guests[num - 1] = name;  //nextLine reads entire line, not just one word.
+        }
+        else {
+            System.out.println("\nError:  There is no guest with that number.");
+        }
+
+    }
+
+    static void removeGuest(){
+        System.out.print("Number: ");
+        int num = scanner.nextInt();
+        if (num >= 1 && num <= 10 && guests[num - 1] != null ) {  //if in range and is valid guest num
+            guests[num - 1] = null;
+            String[] tempArray = new String[guests.length];  //temp array to reposition elements in array
+            int tempIndex = 0;
+            for (int i = 0; i < guests.length; i++) {
+                if (guests[i] != null) {
+                    tempArray[tempIndex] = guests[i];
+                    tempIndex++;
+                }
+            }
+            guests = tempArray;
+        }
+        else {
+            System.out.println("\nError:  There is no guest with that number.");
+        }
+    }
+
+
+    static void insertTestNames(){
+        guests[0] = "Jacob Hughes";
+        guests[1] = "Edward Jones";
+        guests[2] = "Rose Morrison";
+        guests[3] = "Molly Jones";
+        guests[4] = "Christopher Morgan";
     }
 }
